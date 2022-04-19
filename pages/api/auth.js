0dfcc28af01098d1de36bfddf533797dbf519756
Burak Tokak <iletisim@buraktokak.com>
2022-04-19 14:37:30 +0300

    auth endpoint

---
+++ pages/api/auth.js
@@ -0,0 +1,20 @@
+import clientPromise from "../../lib/mongodb";
+
+export default async (req, res) => {
+
+    const { email, password } = req.query
+
+    const client = await clientPromise
+    const db = client.db("UIBucket")
+    const user = await db
+      .collection("User")
+      .find({email: email, password: password})
+      .toArray();
+
+    let authenticated = user.length;
+
+    res.json({
+      authenticated: authenticated == 1 ? true : false,
+      userObject: user[0]
+    });
+};
