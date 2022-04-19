45b5e86545be836c6ea10537443e7c407adee7b9
Burak Tokak <iletisim@buraktokak.com>
2022-04-19 13:56:33 +0300

    update projects

--- pages/api/projects.js
+++ pages/api/projects.js
@@ -2,11 +2,9 @@ import clientPromise from '../../lib/mongodb'
 
 export default async (req, res) => {
   const { db } = await clientPromise();
-  const movies = await db
-    .collection("movies")
+  const projects = await db
+    .collection("Project")
     .find({})
-    .sort({ metacritic: -1 })
-    .limit(20)
     .toArray();
-  res.json(movies);
+  res.json(projects);
 };
