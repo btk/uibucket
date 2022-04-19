f78dc034194d11ac932d342bf839efc75d07003e
Burak Tokak <iletisim@buraktokak.com>
2022-04-19 14:30:25 +0300

    Update users.js

--- pages/api/users.js
+++ pages/api/users.js
@@ -4,7 +4,7 @@ export default async (req, res) => {
     const client = await clientPromise
     const db = client.db("UIBucket")
     const users = await db
-      .collection("Users")
+      .collection("User")
       .find({})
       .toArray();
     res.json(users);
