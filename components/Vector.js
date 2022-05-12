5ced3418c7cdfe9aebcceda410da10a509f2bcf7
Burak Tokak <iletisim@buraktokak.com>
2022-05-12 17:52:27 +0300

    background for accepted

--- components/Vector.js
+++ components/Vector.js
@@ -8,7 +8,8 @@ export default function Vector({ vector }) {
   let isAccepted = typeof vector.accepted != "undefined" && vector.accepted;
 
   return (
-    <div className="vector" style={{ width: 140, height: 160, border: "1px solid #eee", borderRadius: 10, margin: 5, display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
+    <div className="vector" style={{ width: 140, height: 160, border: "1px solid #ddd", borderRadius: 10, margin: 5, display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center",
+    backgroundColor: isAccepted ? "#dff5e7" : "#fff" }}>
       <img src={vector.url} style={{width: 90, height: 90}}/>
       <p>{vector.title}</p>
       {isAccepted && <div className="accepted">✓</div>}
