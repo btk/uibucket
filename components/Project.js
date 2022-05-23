fc1482f6806d937bf0d80af589a9160d7bfd63da
omarmaat <103610170+omarmaat@users.noreply.github.com>
2022-05-23 18:06:52 +0300

    Added project card style

--- components/Project.js
+++ components/Project.js
@@ -2,15 +2,24 @@ import Head from 'next/head'
 import { useState } from 'react'
 import Router from 'next/router'
 import Link from 'next/link'
+import styles from '../styles/project.module.css'
 
 export default function Project({ project }) {
 
   return (
     <Link href={`/project/${project._id}`}>
       <a>
-        <div style={{ width: 250, height: 150, border: "1px solid #aaa", borderRadius: 10, margin: 5}}>
-          <p>{project.name}</p>
-          <small>Team Leader: {project.teamLeader.name}</small>
+        <div className={styles.container}>
+          <div className={styles.card}>
+            <div className={styles.titlebox}>
+              <h1 className={styles.title}>{project.name}</h1>
+              <small className={styles.leader}>Team Leader: {project.teamLeader.name}</small>
+            </div>
+            <div className={styles.contentbox}>
+              <a href='' className={styles.projectlink}>Rename</a>
+              <a href='' className={styles.projectlink}>Delete</a>
+            </div>
+          </div>
         </div>
       </a>
     </Link>
