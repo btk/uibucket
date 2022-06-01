7635e036c508a75e446297431f443df11ea0d9a1
omarmaat <103610170+omarmaat@users.noreply.github.com>
2022-06-01 21:19:23 +0300

    add like/dislike functionality and design with api

--- pages/vector/[[...slug]].js
+++ pages/vector/[[...slug]].js
@@ -11,12 +11,29 @@ import Vector from '../../components/Vector';
 export default function Home({id, assetId}) {
 
   let [project, setProject] = useState({});
+  let [userEmail, setUserEmail] = useState({});
 
   let asset = project.vectors && project.vectors.filter(icon => icon.id == assetId)[0];
   let isAccepted = false;
+  let liked = false;
+  let disliked = false;
+  let numberOfLikes = 0;
+  let numberOfDislikes = 0;
 
   if(project.vectors){
     isAccepted = typeof asset.accepted != "undefined" && asset.accepted;
+    if(asset.likes != undefined && asset.likes.length > 0){
+      if(asset.likes.includes(userEmail)){
+        liked = true;
+      }
+      numberOfLikes = asset.likes.length;
+    }
+    if(asset.dislikes != undefined && asset.dislikes.length > 0){
+      if(asset.dislikes.includes(userEmail)){
+        disliked = true;
+      }
+      numberOfDislikes = asset.dislikes.length;
+    }
   }
 
   let removeConfirm = async () => {
@@ -32,7 +49,44 @@ export default function Home({id, assetId}) {
     Router.reload();
   }
 
+  let addLike = async () => {
+    if(!liked){
+      let liking = await post(`/api/assetLikes`, {id,assetId,userEmail});
+      liked = true;
+      if(disliked){
+        let disliking = await post(`/api/assetRemoveDislike`, {id,assetId,userEmail});
+        disliked = false;
+      }
+      Router.reload();
+    }else{
+      let liking = await post(`/api/assetRemoveLike`, {id,assetId,userEmail});
+      liked = false;
+      Router.reload();
+    }
+  }
+  let addDislike = async () => {
+    if(!disliked){
+      let disliking = await post(`/api/assetDislikes`, {id,assetId,userEmail});
+      disliked = true;
+      if(liked){
+        let liking = await post(`/api/assetRemoveLike`, {id,assetId,userEmail});
+        liked = false;
+      }
+      Router.reload();
+    }else{
+      let disliking = await post(`/api/assetRemoveDislike`, {id,assetId,userEmail});
+      disliked = false;
+      Router.reload();
+    }
+  }
+
   useEffect(() => {
+
+
+      let authString = window.localStorage.getItem("auth");
+      let auth = JSON.parse(authString);
+
+      setUserEmail(auth.userObject.email);
 
       let getProjectInfo = async () => {
         let projectResponse = await get("/api/project", {id: id});
@@ -94,7 +148,15 @@ export default function Home({id, assetId}) {
           </div>
           <div style={{borderLeft: "1px solid #ddd", padding: 20}}>
           <big>Rating:</big><br/><br/>
-          Like (10) Dislike (5)
+
+            <div style={{border:"1px solid black", width:"4vw", height:"6vh",display:"inline-block", cursor:"pointer",verticalAlign:"middle", padding:"2px",borderRadius:5,backgroundColor: liked ? "#69E085" : "000000",marginRight:"1vw"}} onClick={()=>{addLike()}}><img src="https://cdn.svgapi.com/vector/5414/like.svg" style={{width:"100%", height:"100%", verticalAlign:"middle",display:"inline-block"}}/></div>  
+            
+            <div style={{width:"fit-content", height:"6vh",display:"inline-block", cursor:"pointer",verticalAlign:"middle", padding:"2px",borderRadius:5,marginRight:"1vw"}} onClick={()=>{addLike()}}>{numberOfLikes}</div>
+
+            <div style={{border:"1px solid black", width:"4vw", height:"6vh",display:"inline-block", cursor:"pointer",verticalAlign:"middle", padding:"2px",borderRadius:5,backgroundColor: disliked ? "#F02427" : "000000",marginRight:"1vw"}} onClick={()=>{addDislike()}}><img src="https://cdn.svgapi.com/vector/47715/dislike.svg" style={{width:"100%", height:"100%", verticalAlign:"middle",display:"inline-block"}}/></div>  
+
+            <div style={{width:"fit-content", height:"6vh",display:"inline-block", cursor:"pointer",verticalAlign:"middle", padding:"2px",borderRadius:5,marginRight:"1vw"}} onClick={()=>{addLike()}}>{numberOfDislikes}</div>
+
           <br/>
           <br/>
           <br/>
